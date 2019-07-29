e7f789af929726d9a6df512c6ac63a2e9e4ac83d
Mohamed Nashath <masigning@gmail.com>
2019-07-29 20:47:15 +0500

    updated example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,6 @@
-import 'package:flutter/material.dart';
 import 'dart:async';
+import 'package:flutter/material.dart';
 
-import 'package:flutter/services.dart';
 import 'package:nfc_io/nfc_io.dart';
 
 void main() => runApp(MyApp());
@@ -13,64 +12,50 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   bool isReading = false;
+  NfcData data;
   StreamSubscription _subscription;
-  String _platformVersion = 'Unknown';
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await NfcIo.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('NFC IO example app'),
         ),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(isReading ? Icons.stop : Icons.play_arrow),
-          onPressed: () async {
-            print("button press");
-            if (!isReading) {
-              _subscription = NfcIo.startReading.listen((data) {
-                print(data);
-              });
-            } else {
-              if (_subscription != null) {
-                _subscription.cancel();
-                var result = await NfcIo.stopReading;
-                print(result);
-              }
-            }
-            setState(() {
-              isReading = !isReading;
-            });
-          },
+          child: Column(
+            children: <Widget>[
+              Text('Status: ${data?.status ?? "Not ready yet"}'),
+              Text('Card ID: ${data?.id ?? "Unavailable"}'),
+              Text('Card Content: ${data?.content ?? "Unavailable"}'),
+              SizedBox(
+                height: 40,
+              ),
+              RawMaterialButton(
+                child: Icon(isReading ? Icons.stop : Icons.play_arrow),
+                onPressed: () async {
+                  if (!isReading) {
+                    _subscription = NfcIo.startReading.listen((data) {
+                      setState(() {
+                        this.data = data;
+                      });
+                    });
+                  } else {
+                    if (_subscription != null) {
+                      _subscription.cancel();
+                      var result = await NfcIo.stopReading;
+                      setState(() {
+                        this.data = result;
+                      });
+                    }
+                  }
+                  setState(() {
+                    isReading = !isReading;
+                  });
+                },
+              )
+            ],
+          ),
         ),
       ),
     );
